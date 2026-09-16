a568e1689a158198076943861e72f8cd80237e70
jordanianmartin1-create <jordan.ian.martin1@gmail.com>
2026-09-16 14:37:13 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
+const CACHE_NAME = 'care-digipad-v1';
+const ASSETS = [
+    './index.html',
+    './manifest.json',
+    './icon.png'
+];
 
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((cachedResponse) => {
+            return cachedResponse || fetch(e.request);
+        })
+    );
+});
